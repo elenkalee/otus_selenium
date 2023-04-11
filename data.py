01ec529f1e34f4c00a4979f3018242242fc957c8
elenkalee <elena.lipukhina@gmail.com>
2023-04-11 11:16:08 +0300

    Add Logger and Allure

---
+++ data.py
@@ -0,0 +1,13 @@
+TEST_USER = {
+    "firstname": "test_name",
+    "lastname": "test_lastname",
+    "email": "test@email.ru",
+    "telephone": "+79786567889",
+    "password": "test_password",
+    "input_confirm": "test_password"
+}
+
+ADMIN = {
+    "username": "user",
+    "password": "bitnami"
+}
